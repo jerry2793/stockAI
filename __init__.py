bef739e767e0c623c371577485442d911e97fb9c
KAI-SEK LIM <kai-sek_lim@keysight.com>
2020-09-19 17:52:05 -0700

    sa

---
+++ __init__.py
@@ -0,0 +1,22 @@
+import json
+import requests 
+import yfinance as yf
+from yahoo_fin import stock_info as si
+import pymongo
+
+def main(stock, startDate, endDate):
+
+    msft = yf.download("SPY "+stock, start=startDate, end=endDate)
+
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    stockdb = myclient["stockdb"]
+
+    #collectionNames =  myclient["stockdb"].list_collection_names(session=None, filter=None)
+
+    stockCol = stockdb[stock]  
+    
+    records = json.loads(msft.to_json()).values()
+    stockCol.insert(records)
+    
+
+main("AAPL", "2017-01-01", "2017-04-30")
--- get_data.py
+++ get_data.py
@@ -1,17 +1,34 @@
 from requests import get
-from datetime import datetime
+from datetime import datetime, timedelta 
+import sqlite3
 
+# all you need to do is to change the STOCKS with appr symbols conn = sqlite3.connect('data.db') c = conn.cursor() KEYS = ['c','h','l','o','t','v'] STOCKS = ['aapl','AMZN'.lower(),'NVDA'.lower()]
+for i in STOCKS:
+    try:
+        print(f'creating table: {i}')
+        c.execute(f"""CREATE TABLE {i} (
+            c real,
+            h real,
+            l real,
+            o real,
+            t real,
+            v real
+        )""")
+    except:
+        pass
 
 def historical_save_gen(json):
     # generates rows in the correct (corresponding) format to save
+    # print(json)
     for i in range(len(json['t'])):
         row = []
         # print('main loop')
+        # print(json.keys())
         for k in json.keys():
-            print(k)
+            # print(k)
             if k == 's':
                 continue
-            else: 
+            else:
                 # print('continued')
                 row.append(list(json[k])[i])
                 # print(k)
@@ -20,15 +37,21 @@ def historical_save_gen(json):
 
 def save(r):
     # saves the row
-    pass
+    cols = ''
+    for i in KEYS:
+        cols += f'{i},'
+    values = ''
+    for i in r:
+        values += f'{i},'
+    c.execute(f"INSERT INTO aapl ({cols.strip(',')}) VALUES ({values.strip(',')})")
 
 
 def getHistoricalData(s,start,finish,res=1):
     '''
-    params: 
+    params:
     s: symbol (stock) of desire (str),
     start: (unix timestamp) date from (1572651390),
-    finish: (unix timestamp) date to (1572910590),
+    finish: (unix timestamp) date to  (1572910590),
     res: the resolution of the data
     '''
     params = {
@@ -38,17 +61,32 @@ def getHistoricalData(s,start,finish,res=1):
         'to': finish,
         'token': 'btbetpn48v6tim9d4ve0'
     }
-    url = 'http://finnhub.io/api/v1/stock/candle'
+    url = 'https://urldefense.com/v3/__http://finnhub.io/api/v1/stock/candle__;!!I5pVk4LIGAfnvw!x6oGMmHXru6nL2xgMlJI0A7DCuC2Pxwxm1ZodRd35F7WGpUyVK6S6GtOmLiGwn8y6zCM$ '
     r = get(url,params=params)
+    # print(r.status_code)
 
     return dict(r.json())
 
 
-def main():
+def date_gen(start,finish):
+    '''
+    example:
+    start: 2019,5,6
+    finish: 2020,4,8
+    returns: timestamp of start (earlier) to finish (later)
+    '''
+    x = finish - start
+    for i in range(x.days):
+        s = finish - timedelta(days=i)
+        f = finish - timedelta(days=i-1)
+        yield [s.timestamp(),f.timestamp()]
+
+
+def main(q,start,finish):
     r = getHistoricalData(
-                'AAPL',
-                datetime(2020,9,9).timestamp(),
-                datetime(2020,9,8).timestamp()
+                q.upper(),
+                start,
+                finish
                 )
     # print(r)
     # j = 0
@@ -56,12 +94,24 @@ def main():
         # print(i)
 
         save(i)
-
+        print(i)
         # j += 1
-        # if j == 10: 
+        # if j == 10:
         #     break
         # break
 
 
 if __name__ == '__main__':
-    main()
+    for i in STOCKS:
+        print(i.upper())
+        for j in date_gen(datetime(2019,9,17),datetime(2020,8,30)):
+            # print(j)
+            main(i.upper(),j[0],j[1])
+
+
+    # for i in date_gen(datetime(2020,7,5),datetime(2020,8,6)):
+    #     print(str(i))
+
+
+        conn.commit()
+    conn.close()
